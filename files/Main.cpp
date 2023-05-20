5a7109b454a530379ed50bdfd988ebf4809bb20a
borna145 <borna02010@gmail.com>
2023-05-20 22:59:59 +0200

    included water and food source header files

--- files/Main.cpp
+++ files/Main.cpp
@@ -4,6 +4,8 @@
 #include "Virus.hpp"
 #include "Human.hpp"
 #include "/home/borna/C++/imgui-1.89.5/imgui.h"
+#include "Watersource.hpp"
+#include "Foodsource.hpp"
 int main(){
     std::cout << "Type A = airborne\n";
     std::cout << "Version 1.3";
