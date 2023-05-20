1b66638471508607e1b8f5b77309388f2aaf3c51
borna145 <borna02010@gmail.com>
2023-05-20 16:32:41 +0200

    added danger levels

--- Virus.hpp
+++ Virus.hpp
@@ -3,5 +3,17 @@ class virus{
     public:
         char type;
         int generation;
-        
+        char dangerlvl;
 };
+
+
+/*types: A (airborne)
+B (touch)
+c (body fluids)
+d (mixed) */
+
+/*danger lvls:
+ A (Low Danger)
+ B (Medium Danger)
+ C (Severe Danger)
+ D (Maximum Danger)*/
